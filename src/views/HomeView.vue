933d047c5af0153741174317f8c769c69200ee98
NEPHERAovo <nepheramusic@gmail.com>
2022-10-14 22:48:45 +0800

    fix typescript bugs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,15 +7,16 @@
             <div class="w-auto rotate-45 flex mx-24">
                 <div v-for="count in 6">
                     <div v-for="count2 in 6">
-                        <div v-bind:id="(count-1)*6+count2" @click="clickButton((count-1)*6+count2)" v-if="(count==1 && count2<3) || (count==2 && count2<4) || (count==3 && count2>1 && count2<5)
+                        <div v-bind:id="((count-1)*6+count2).toString()" @click="clickButton((count-1)*6+count2)" v-if="(count==1 && count2<3) || (count==2 && count2<4) || (count==3 && count2>1 && count2<5)
                         || (count==4 && count2>2 && count2<6) || (count==5 && count2>3) || (count==6 && count2>4) "
                             class="misc h-[3.25rem] w-[3.25rem] m-[0.3rem] mx-[0.15rem] border border-border-color bg-cyan-un hover:bg-cyan-hover cursor-pointer">
                         </div>
-                        <div v-bind:id="(count-1)*6+count2" @click="clickButton((count-1)*6+count2)" v-else-if="(count==1 && count2>=3 && count2<=6) || (count==2 && count2>=4) || (count==3 && count2>=5)
-                        || (count==4 && count2==6)"
+                        <div v-bind:id="((count-1)*6+count2).toString()" @click="clickButton((count-1)*6+count2)"
+                            v-else-if="(count==1 && count2>=3 && count2<=6) || (count==2 && count2>=4) || (count==3 && count2>=5)
+                            || (count==4 && count2==6)"
                             class="drums h-[3.25rem] w-[3.25rem] m-[0.3rem] mx-[0.15rem] border border-border-color bg-blue-un hover:bg-blue-hover cursor-pointer">
                         </div>
-                        <div v-bind:id="(count-1)*6+count2" @click="clickButton((count-1)*6+count2)" v-else
+                        <div v-bind:id="((count-1)*6+count2).toString()" @click="clickButton((count-1)*6+count2)" v-else
                             class="bass h-[3.25rem] w-[3.25rem] m-[0.3rem] mx-[0.15rem] border border-border-color bg-red-un hover:bg-red-hover cursor-pointer">
                         </div>
                     </div>
@@ -30,39 +31,41 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-const active = Array(36).fill(0);
-const audios = []
+const active: Array<number> = Array(36).fill(0);
+const audios: any = [];
 for (let i = 1; i <= 36; i++) {
     let temp = new Audio(`/sound/${i}.ogg`);
     temp.loop = true;
     audios.push(temp);
 }
-const clickButton = (id) => {
-    let button = document.getElementById(id);
+const clickButton = (id: number) => {
+    let button: HTMLElement | null = document.getElementById(id.toString());
     id = id - 1;
-    if (active[id] === 0) {
-        audios[id].play();
-        active[id] = 1;
-        if (button.classList.contains("misc")) {
-            button.classList.add("blinking-green");
-        } else if (button.classList.contains("drums")) {
-            button.classList.add("blinking-blue");
+    if (button) {
+        if (active[id] === 0) {
+            audios[id].play();
+            active[id] = 1;
+            if (button.classList.contains("misc")) {
+                button.classList.add("blinking-green");
+            } else if (button.classList.contains("drums")) {
+                button.classList.add("blinking-blue");
+            } else {
+                button.classList.add("blinking-red");
+            }
         } else {
-            button.classList.add("blinking-red");
-        }
-    } else {
-        active[id] = 0;
-        audios[id].pause();
-        audios[id].currentTime = 0;
-        if (button.classList.contains("misc")) {
-            button.classList.remove("blinking-green");
-        } else if (button.classList.contains("drums")) {
-            button.classList.remove("blinking-blue");
-        } else {
-            button.classList.remove("blinking-red");
+            active[id] = 0;
+            audios[id].pause();
+            audios[id].currentTime = 0;
+            if (button.classList.contains("misc")) {
+                button.classList.remove("blinking-green");
+            } else if (button.classList.contains("drums")) {
+                button.classList.remove("blinking-blue");
+            } else {
+                button.classList.remove("blinking-red");
+            }
         }
     }
+
 
 }
 </script>
